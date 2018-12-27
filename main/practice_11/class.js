fc5706dd7b93a83023bcb05d795ff74aae9912f8
Grace <lunay0325@gmail.com>
2018-12-28 02:58:42 +0800

    Add methods to notify when appending member and assigning leader

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,38 @@
-
+class Class {
+  constructor(num) {
+    this.number = num;
+  }
+  getDisplayName() {
+    return `Class ${this.number}`;
+  }
+  assignLeader(student) {
+    if (student.klass.number === this.number) {
+      this.leader = student;
+      if (this.assignLeaderListener) {
+        console.log(`I am ${this.assignLeaderListener.name}. I know ${student.name} become Leader of Class ${this.number}.`);
+      }
+    } else {
+      console.log('It is not one of us.');
+    }
+  }
+  appendMember(student) {
+    student.klass = this;
+    if (this.joinListener) {
+      console.log(`I am ${this.joinListener.name}. I know ${student.name} has joined Class ${this.number}.`);
+    }
+  }
+  isIn(student) {
+    return student.klass.number === this.number;
+  }
+  registerAssignLeaderListener(teacher) {
+    if (teacher.klasses.map(klass => klass.number).includes(this.number)) {
+      this.assignLeaderListener = teacher;
+    }
+  }
+  registerJoinListener(teacher) {
+    if (teacher.klasses.map(klass => klass.number).includes(this.number)) {
+      this.joinListener = teacher;
+    }
+  }
+}
+module.exports = Class;
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,20 @@
-
+import Person from "./person.js";
+class Teacher extends Person {
+  constructor(id, name, age, klassArr) {
+    super(id, name, age);
+    this.klasses = klassArr;
+  }
+  basic_introduce() {
+    return `${super.introduce()} I am a Teacher.`;
+  }
+  introduce() {
+    return `${this.basic_introduce()} I teach ${this.klasses ? `Class ${this.klasses.map(klass => klass.number).join(', ')}.` : 'No Class.'}`;
+  }
+  introduceWith(stu) {
+    return `${this.basic_introduce()} I${stu.klass.number === this.klass.number ? "" : " don't"} teach ${stu.name}.`;
+  }
+  isTeaching(stu) {
+    return Boolean(this.klasses.filter(klass => klass.isIn(stu)));
+  }
+}
+module.exports = Teacher;
