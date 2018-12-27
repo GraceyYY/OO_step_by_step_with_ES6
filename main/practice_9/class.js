58425aee5b1863f8ce9ee38384e674080e41c7c8
Grace <lunay0325@gmail.com>
2018-12-28 01:58:04 +0800

    Add a method for Class class so that it can append members

--- main/practice_9/class.js
+++ main/practice_9/class.js
@@ -1,1 +1,19 @@
-
+class Class {
+  constructor(num) {
+    this.number = num;
+  }
+  getDisplayName() {
+    return `Class ${this.number}`;
+  }
+  assignLeader(student) {
+    if (student.klass === this) {
+      this.leader = student;
+    } else {
+      console.log('It is not one of us.');
+    }
+  }
+  appendMember(student) {
+    student.klass = this;
+  }
+}
+module.exports = Class;
--- main/practice_9/student.js
+++ main/practice_9/student.js
@@ -1,1 +1,11 @@
-
+import Person from './person.js';
+class Student extends Person {
+  constructor(id, name, age, klassObj) {
+    super(id, name, age);
+    this.klass = klassObj;
+  }
+  introduce() {
+    return `${super.introduce()} I am a Student. ${this.klass.leader ? `I am Leader of Class ${this.klass.number}.` : `I am at Class ${this.klass.number}.`}`;
+  }
+}
+module.exports = Student;
